21ee9563b4d8a6cd75a309f54f44345cd6020982
KimigaiiWuyi <444835641@qq.com>
2024-07-05 02:51:40 +0800

    ✨ 添加角色别名文件

---
+++ ZZZeroUID/utils/name_convert.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from typing import Dict, List
+
+from msgspec import json as msgjson
+
+ALIAS_LIST = Path(__file__).parent / 'alias'
+CHAR_ALIAS = ALIAS_LIST / 'alias.json'
+
+
+with open(CHAR_ALIAS, 'r', encoding='UTF-8') as f:
+    char_alias_data = msgjson.decode(f.read(), type=Dict[str, List[str]])
+
+
+def alias_to_char_name(char_name: str) -> str:
+    for i in char_alias_data:
+        if (char_name in i) or (char_name in char_alias_data[i]):
+            return i
+    return char_name
