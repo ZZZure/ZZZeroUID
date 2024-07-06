2cb98a63271e54bb36067ae00f73df3b5716d46f
KimigaiiWuyi <444835641@qq.com>
2024-07-06 11:00:30 +0800

    ✨ 更新`1.0.0`版本的map文件

---
+++ ZZZeroUID/tools/data_to_map.py
@@ -0,0 +1,143 @@
+import sys
+import json
+import asyncio
+from pathlib import Path
+
+import httpx
+
+sys.path.append(str(Path(__file__).parents[5]))
+sys.path.append(str(Path(__file__).parents[2]))
+
+__package__ = 'ZZZeroUID.tools'
+
+from ..version import ZZZero_version  # noqa: E402
+
+R_PATH = Path(__file__).parents[0]
+ZZZ_DATA = R_PATH / 'zzz_data'
+MAP_PATH = Path(__file__).parents[1] / 'utils' / 'map'
+
+if not ZZZ_DATA.exists():
+    ZZZ_DATA.mkdir()
+
+version = ZZZero_version
+raw_data = {}
+weapon_data = {}
+partner_data = {}
+gacha_data = {}
+avatar_data = {}
+
+
+WeaponId2SpriteFile = f'WeaponId2Sprite_{version}.json'
+PartnerId2DataFile = f'PartnerId2SpriteId_{version}.json'
+GachaId2SpriteIdFile = f'GachaId2SpriteId_{version}.json'
+
+
+def gen_weapon_id_to_sprite():
+    print('[正在执行] gen_weapon_id_to_sprite')
+    weapon_id_to_sprite = {}
+    for item in weapon_data['GMNCBMLIHPE']:
+        weapon_id = item['NOJCFGOCGBI']
+        sprite_file = item['LAFKHMCKNIO']
+        weapon_id_to_sprite[weapon_id] = sprite_file
+    with open(MAP_PATH / WeaponId2SpriteFile, 'w', encoding='UTF-8') as f:
+        json.dump(weapon_id_to_sprite, f, indent=4, ensure_ascii=False)
+    print('[执行完成] gen_weapon_id_to_sprite')
+
+
+def gen_gacha_id_to_sprite():
+    print('[正在执行] gen_gacha_id_to_sprite')
+    gacha_id_to_sprite = {}
+    for item in gacha_data['GMNCBMLIHPE']:
+        gacha_id = item['NOJCFGOCGBI']
+        sprite_file = item['FAIIOENDLMC'].split('/')[-1]
+        gacha_id_to_sprite[gacha_id] = sprite_file
+    with open(MAP_PATH / GachaId2SpriteIdFile, 'w', encoding='UTF-8') as f:
+        json.dump(gacha_id_to_sprite, f, indent=4, ensure_ascii=False)
+    print('[执行完成] gen_gacha_id_to_sprite')
+
+
+def gen_partner_id_to_data():
+    print('[正在执行] gen_partner_id_to_data')
+    partner_id_to_data = {}
+    for item in partner_data['GMNCBMLIHPE']:
+        partner_id = item['EJLCDDCBPAE']
+        if partner_id not in partner_id_to_data:
+            partner_id_to_data[partner_id] = {}
+        sprite_id = item['GPOKHDCGAHN'].replace('IconRole', '')
+        if item['FNEMGJINKNN'] not in raw_data:
+            for av in avatar_data['GMNCBMLIHPE']:
+                if av['HBKDOIKGNDE'] == partner_id:
+                    if av['DIIDBBGLDOL'] in raw_data:
+                        texture_id = av['DIIDBBGLDOL']
+                        break
+            else:
+                continue
+        else:
+            texture_id = item['FNEMGJINKNN']
+        partner_name = raw_data[texture_id]
+        full_name = raw_data[f"{texture_id}_FullName"]
+        if 'AKEMNEKJMKD' not in item:
+            continue
+        en_name = item['AKEMNEKJMKD']
+
+        partner_id_to_data[partner_id]['sprite_id'] = sprite_id
+        partner_id_to_data[partner_id]['name'] = partner_name
+        partner_id_to_data[partner_id]['full_name'] = full_name
+        partner_id_to_data[partner_id]['en_name'] = en_name
+
+    with open(MAP_PATH / PartnerId2DataFile, 'w', encoding='UTF-8') as f:
+        json.dump(partner_id_to_data, f, indent=4, ensure_ascii=False)
+    print('[执行完成] gen_partner_id_to_data')
+
+
+async def download_new_file():
+    print('[正在执行] download_new_file')
+    URL = 'https://raw.githubusercontent.com/Dimbreath/ZenlessData/master'
+    url_list = [
+        f'{URL}/FileCfg/WeaponTemplateTb.json',
+        f'{URL}/FileCfg/PartnerConfigTemplateTb.json',
+        f'{URL}/FileCfg/GachaItemResourceTemplateTb.json',
+        f'{URL}/FileCfg/AvatarBaseTemplateTb.json',
+        f'{URL}/TextMap/TextMapTemplateTb.json',
+    ]
+
+    async with httpx.AsyncClient() as client:
+        for url in url_list:
+            file_name = url.split('/')[-1]
+            response = await client.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                with open(ZZZ_DATA / file_name, 'w', encoding='UTF-8') as f:
+                    json.dump(data, f, indent=4, ensure_ascii=False)
+                print(f'[执行完成] 文件已成功下载并保存为{ZZZ_DATA / file_name}')
+            else:
+                print(f'下载失败，状态码为{response.status_code}')
+
+
+async def main():
+    # await download_new_file()
+    global raw_data
+    global weapon_data
+    global partner_data
+    global gacha_data
+    MAP = {
+        'TextMapTemplateTb.json': raw_data,
+        'WeaponTemplateTb.json': weapon_data,
+        'PartnerConfigTemplateTb.json': partner_data,
+        'GachaItemResourceTemplateTb.json': gacha_data,
+        'AvatarBaseTemplateTb.json': avatar_data,
+    }
+    try:
+        for k in MAP:
+            with open(ZZZ_DATA / k, 'r', encoding='UTF-8') as f:
+                MAP[k].update(json.load(f))
+
+        gen_weapon_id_to_sprite()
+        gen_partner_id_to_data()
+        gen_gacha_id_to_sprite()
+
+    except FileNotFoundError:
+        print('未找到TextMapCHS.json文件，停止转换！')
+
+
+asyncio.run(main())
--- ZZZeroUID/version.py
+++ ZZZeroUID/version.py
@@ -1,2 +1,2 @@
 ZZeroUID_version = '1.0.0'
-ZZZero_version = '0.9.3'
+ZZZero_version = '1.0.0'
