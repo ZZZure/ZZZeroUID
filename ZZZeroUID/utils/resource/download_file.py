a3378dd3f409c058f7915bc690ad7c7c6d0ea654
KimigaiiWuyi <444835641@qq.com>
2024-08-14 18:08:56 +0800

    🎨 优化武器图片获取

--- ZZZeroUID/utils/resource/download_file.py
+++ ZZZeroUID/utils/resource/download_file.py
@@ -45,7 +45,14 @@ async def get_weapon(weapon_id: Union[str, int]) -> Image.Image:
     img = Image.new("RGBA", (400, 400))
     if str(weapon_id) in weapon_data:
         weapon_sprite = weapon_data[str(weapon_id)]
-        weapon = Image.open(WEAPON_PATH / f"{weapon_sprite}.png")
+        path_1 = WEAPON_PATH / f"{weapon_sprite}.png"
+        path_2 = WEAPON_PATH / f"{weapon_sprite}_High.png"
+        if path_2.exists():
+            weapon = Image.open(path_2)
+        elif path_1.exists():
+            weapon = Image.open(path_1)
+        else:
+            return img
         weapon = weapon.convert("RGBA")
         x, y = weapon.size
         img.paste(weapon, (200 - x // 2, 200 - y // 2), weapon)
