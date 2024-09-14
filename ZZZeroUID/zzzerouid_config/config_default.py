17e0c41c6eea9cd5a0b7cd8fcf2916656db21e4a
KimigaiiWuyi <444835641@qq.com>
2024-09-14 22:29:35 +0800

    ✨ `zzzmr`默认使用小组件API, 可在控制台配置默认源

--- ZZZeroUID/zzzerouid_config/config_default.py
+++ ZZZeroUID/zzzerouid_config/config_default.py
@@ -2,7 +2,6 @@ from typing import Dict
 
 from gsuid_core.utils.plugins_config.models import (
     GSC,
-    GsIntConfig,
     GsStrConfig,
     GsBoolConfig,
     GsListStrConfig,
@@ -33,6 +32,11 @@ CONFIG_DEFAULT: Dict[str, GSC] = {
     'SchedEnergyPush': GsBoolConfig(
         '定时检查体力',
         '开启后每隔半小时检查一次开启推送的人的体力状态',
+        True,
+    ),
+    'WidgetResin': GsBoolConfig(
+        '体力使用组件API',
+        '开启后zzzmr功能将转为调用组件API, 可能数据不准、刷新过慢',
         True,
     ),
     "CrazyNotice": GsBoolConfig(
