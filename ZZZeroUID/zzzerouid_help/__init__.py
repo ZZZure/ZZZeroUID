01e69dc96081833c940c3861fe462a691062809b
KimigaiiWuyi <444835641@qq.com>
2024-09-21 21:25:18 +0800

    ✨ 适配`GsCore`全新的帮助系统`zzz帮助`, 注册至`帮助`

--- ZZZeroUID/zzzerouid_help/__init__.py
+++ ZZZeroUID/zzzerouid_help/__init__.py
@@ -1,9 +1,11 @@
+from PIL import Image
 from gsuid_core.sv import SV
 from gsuid_core.bot import Bot
 from gsuid_core.models import Event
 from gsuid_core.logger import logger
+from gsuid_core.help.utils import register_help
 
-from .get_help import get_help
+from .get_help import ICON, get_help
 from ..utils.zzzero_prefix import PREFIX
 
 sv_zzz_help = SV('zzz帮助')
@@ -13,3 +15,6 @@ sv_zzz_help = SV('zzz帮助')
 async def send_help_img(bot: Bot, ev: Event):
     logger.info('开始执行[zzz帮助]')
     await bot.send(await get_help())
+
+
+register_help('ZZZeroUID', f'{PREFIX}帮助', Image.open(ICON))
